4e9be7ed31649cf15a61e00952abe2ee260ef7bc
Tristan011 <tristan.keena@student.pxl.be>
2020-12-13 13:08:03 +0100

    php and composer test

--- roles/php/templates/config.php
+++ roles/php/templates/config.php
@@ -1,9 +1,9 @@
 <?php
     require('vendor/autoload.php');
     // Define mysql server settings
-    $serverName = '{{ rds_db_name }}';
-    $username = '{{ rds_db_name }}';
-    $password = '{{ rds_db_name }}';
+    $serverName = '{{ rds_db_hostname }}';
+    $username = '{{ rds_db_user }}';
+    $password = '{{ rds_db_password }}';
     $dbName = '{{ rds_db_name }}';
 
     // Create a new mysqli connection. Remember to enable this in php.ini !!
